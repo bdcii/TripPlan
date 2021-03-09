ace373a6e5b191b15de742f2fecb1ce49300b3e0
Kristen Picard <kristenpicard93@gmail.com>
2021-03-09 11:16:28 -0500

    Adding Weather forecast on ciy selection

--- flightapi.js
+++ flightapi.js
@@ -1,43 +1,39 @@
-const flightAPIKey = "4b42a0d39fec9c53fd432a1594cb3bf5"
-const flightData = document.querySelector('#flight-data');
-const searchButtonFlight = document.querySelector('#search-button');
-const userInputFlight = document.querySelector('#city-input');
+// const flightAPIKey = "4b42a0d39fec9c53fd432a1594cb3bf5"
+// const flightData = document.querySelector('#flight-data');
+// const searchButtonFlight = document.querySelector('#search-button');
+// const userInputFlight = document.querySelector('#city-input');
 
 
+// function displayFlights(event) {
+//     event.preventDefault();
 
-
-// This function is a container to call other functions
-function displayFlights(event) {
-    event.preventDefault();
-
-    fetch(`http://api.aviationstack.com/v1/cities?access_key=4b42a0d39fec9c53fd432a1594cb3bf5`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+//     fetch(`http://api.aviationstack.com/v1/cities?access_key=4b42a0d39fec9c53fd432a1594cb3bf5`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
             
-        })
-};
+//         })
+// };
 
-searchButton.addEventListener('click', displayFlights);
+// searchButton.addEventListener('click', displayFlights);
 
 
-
-$.ajax({
-    url: 'http://api.aviationstack.com/v1/cities',
-    data: {
-      access_key: flightAPIKey
-    },
-    dataType: 'json',
-    success: function(apiResponse) {
-      if (Array.isArray(apiResponse['results'])) {
-        apiResponse['results'].forEach(flight => {
-          if (!flight['live']['is_ground']) {
-            console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
-                `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
-                `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
-          }
-        });
-      }
-    }
-  });
+// $.ajax({
+//     url: 'http://api.aviationstack.com/v1/cities',
+//     data: {
+//       access_key: flightAPIKey
+//     },
+//     dataType: 'json',
+//     success: function(apiResponse) {
+//       if (Array.isArray(apiResponse['results'])) {
+//         apiResponse['results'].forEach(flight => {
+//           if (!flight['live']['is_ground']) {
+//             console.log(`${flight['airline']['name']} flight ${flight['flight']['iata']}`,
+//                 `from ${flight['departure']['airport']} (${flight['departure']['iata']})`,
+//                 `to ${flight['arrival']['airport']} (${flight['arrival']['iata']}) is in the air.`);
+//           }
+//         });
+//       }
+//     }
+//   });
 

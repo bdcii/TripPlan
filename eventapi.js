80a7088a9366698f309c93005c3c6b8f8ae6fe88
Kristen Picard <kristenpicard93@gmail.com>
2021-03-10 21:49:33 -0500

    Getting only next 8 days of data

--- eventapi.js
+++ eventapi.js
@@ -10,14 +10,45 @@ function displayEvents(event) {
     fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=gE0vIThWEHkWajQ5c8zJtOurSipLrsoy&city=${userInputEvent.value}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log(data._embedded.events[0].images[0]);
-            for (i=0; i < data._embedded.events.length; i++){
-                var image = document.createElement("div");
-                image.innerHTML = `<img src = ${data._embedded.events[i].images[0].url} style="height:200px"></img>`;
-                eventData.appendChild(image);
+            var today = new Date();
+            for (i = 0; i < data._embedded.events.length; i++) {
+                var date = data._embedded.events[i].dates.start.localDate;
+                if (isWithinTimeSpan(date, today, 8)) {
+
+                    var div = document.createElement("div");
+                    // Add more HTML here Kristen
+                    div.innerHTML = `<img src = ${data._embedded.events[i].images[0].url} style="height:200px"></img><br><br>`;
+
+                    eventData.appendChild(div);
+                }
             };
         });
 };
+
+function isWithinTimeSpan(eventDate, currentDate, numberOfDaysAfterCurrentDate) {
+    for (days = 0; days <= numberOfDaysAfterCurrentDate; days++) {
+        var nextDate = getNewDateAfterNDays(currentDate, days);
+        if (nextDate == eventDate) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function getNewDateAfterNDays(currentDate, days) {
+    var newDate = new Date(currentDate);
+    newDate.setDate(newDate.getDate() + days);
+    var formattedNewDate = formatDate(newDate);
+    return formattedNewDate;
+};
+
+
+function formatDate(date) {
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = date.getFullYear();
+    formatedDate = yyyy + '-' + mm + '-' + dd;
+    return formatedDate;
+}
 
 searchButtonEvent.addEventListener('click', displayEvents);

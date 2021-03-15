6ea5a3cd749780ead205d68c77f9fc65897683b6
Samuel Maddox <18106785+SamuelMaddox@users.noreply.github.com>
2021-03-14 20:21:13 -0400

    Samuel Maddox Comments

--- weatherapi.js
+++ weatherapi.js
@@ -1,3 +1,22 @@
+// ===================
+// Samuel Maddox Notes
+// ===================
+// It is advisable to limit the number of characters on any given line. 80 or 100 are standards I've
+// Seen. I helps when viewing code on smaller screens, or side by side. VSCode has a setting to add 
+// a verticle ruler where you want to stop your lines. Search your setting for "ruler" and you'll 
+// find it. It may have to be edited in the JSON version of your settings. Google if interested.
+//
+// I'm also seeing you use both vanilla JavaScript DOM functions and JQuery DOM functions. I'd
+// recomend choising one or the other. It would be easy for bugs to come into play because you 
+// selected an element using JavaScript DOM and tried to edit it using JQuery DOM. It also can make
+// code harder to follow because my head is having to context switch betwen the two.
+//
+// NOTE: All code I've provided below has not been tested and may have syntax errors. Use it more
+// as pseudocode trying to get a point across rather than 100% functional code.
+// ===================
+// End Samuel Notes
+// ===================
+
 const weatherAPIKey = "c979757ff3364fdbc7788b954c2541a8";
 const searchButton = document.querySelector('#search-button');
 const datePicker = document.querySelector('.date-picker');
@@ -6,13 +25,23 @@ const userInput = document.querySelector('#city-input');
 var cityList = [];
 var cityStorage = localStorage;
 
-// When the user searches for a city, this function adds it to the list of previously searched cities in the form of a clickale button.
+// When the user searches for a city, this function adds it to the list of previously searched 
+// cities in the form of a clickale button.
 function retrievePrevSearch() {
-    if (cityStorage.getItem("pastCitySearch") != undefined) {
+    // ===================
+    // Samuel Maddox Notes
+    // ===================
+    // It's better to use the strictily not equal operator (!==). Google the following:
+    // "javascript loose vs strict equality"
+    // ===================
+    // End Samuel Notes
+    // ===================
+    if (cityStorage.getItem("pastCitySearch") !== undefined) {
         cityList = JSON.parse(cityStorage.getItem("pastCitySearch"));
 
         var newRecentSearchLink = $("<a href=\"\#\"></a>");
-        // cityList.Length-1 prevents a city for showing up in the previously searched list multiple times
+        // cityList.Length-1 prevents a city for showing up in the previously searched list 
+        // multiple times
         newRecentSearchLink.attr("data-city", cityList[cityList.length - 1]);
         newRecentSearchLink.text(cityList[cityList.length - 1]);
         newRecentSearchLink.attr("id", 'recentSearchItemID');
@@ -26,11 +55,38 @@ function savePrevSearch() {
     cityStorage.setItem("pastCitySearch", JSON.stringify(cityList));
 };
 
-// This function fetches the API based on the user city input and then calls the week's forecast function.
+// This function fetches the API based on the user city input and then calls the week's forecast 
+// function.
 function getCityWeather(eventOrString) {
 
-    //We run the getCityWeather function two ways in our js file.  One is via the user input string of the city name, and one is on the click event when the city storage button is pushed.
-    //This if/else statement determines how to "get" the city name (based on whether it is accessed via the event or string) when getCityWeather is called.
+    // ===================
+    // Samuel Maddox Notes
+    // ===================
+    // Good job explaining why this if/else statement is needed. That certainly helps since this is 
+    // an odd scenario. I think a better way to do this would be a 3 function solution.
+    // Function 1: Strictly a wrapper for the fetch call with a city parameter
+        function getCityWeather(city) {
+            fetch(/*...city param used here...*/).then(/*...*/);
+        }
+    // Function 2: Handle event listner and call Function 1.
+        function handleEvent(event) {
+            event.preventDefault();
+            getCityWeather(userInput.value);
+        }
+    // Function 3: City Storage Button (this one I don't think you got around to flushing out)
+        function pullFromLocalStorage() {
+            // Get City String from local storage, or some other way, and save to variable 'city'
+            getCityWeather(city);
+        }
+    // ===================
+    // End Samuel Notes
+    // ===================
+
+
+    // We run the getCityWeather function two ways in our js file.  One is via the user input string 
+    // of the city name, and one is on the click event when the city storage button is pushed.
+    // This if/else statement determines how to "get" the city name (based on whether it is 
+    // accessed via the event or string) when getCityWeather is called.
     if (eventOrString instanceof Event) {
         var city = userInput.value;
         eventOrString.preventDefault();
@@ -61,6 +117,70 @@ function getCityWeather(eventOrString) {
 };
 
 // Week forecast function that is called above within the above function of the search event listener
+// ===================
+// Samuel Maddox Notes
+// ===================
+// HINT: If you notice yourself repeating similiar lines of code, there is probably a way to 
+// simplify it. The function you wrote below my comment could be simplified to the following (and 
+// some of the html in index.html could also be removed)
+    function buildWeatherCards(data) {
+        // NOTE: () => {...} is called an arrow function (Google it). It's the same as \
+        // writing function() {...}
+        // NOTE: arrays have functions. the other way we could have done this is using a for loop.
+        data.daily.forEach((day) => {
+            var date = moment.unix(day.dt).format("M/DD/YYYY");
+            var icon = day.weather[0].icon;
+            var temp = day.temp.day;
+            var humidity = day.humidity;
+
+            // Create Data Elements
+            var headingEl = document.createElement('H5').setAttribute('class', 'card-title date');
+            headingEl.innerHTML = date;
+            
+            var iconEl = document.createElement('P').setAttribute('class', 'card-text');
+            iconEl.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`;
+            
+            var tempEl = document.createElement('P').setAttribute('class', 'card-text');
+            tempEl.innerHTML = `Temperature:<br> ${temp} °F`;
+            
+            var humidityEl = document.createElement('P').setAttribute('class', 'card-text');
+            humidityEl.innerHTML = `Humidity:<br> ${humidity}%`;
+
+            // Create weatherCardBody and append data elements
+            var weatherCardBody = document.createElement('DIV')
+            weatherCardBody
+                .setAttribute('class', 'card-body')
+                .appendChild(headingEl)
+                .appendChild(iconEl)
+                .appendChild(tempEl)
+                .appendChild(humidityEl)
+
+            // Create weatherCard list item and append weatherCardBody
+            var weatherCard = document.createElement('LI')
+                .setAttribute('class', 'card')
+                .appendChild(weatherCardBody);
+
+            // Append weatherCard to card deck list
+            document.getElementById('weather-card')
+                .getElementsByClassName('card-deck')[0]
+                .appendChild(weatherCard);
+        });
+    }   
+
+    function displayWeekForecast(data) {
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=imperial&appid=${weatherAPIKey}`)
+            .then(response => response.json())
+            .then(buildWeatherCards);
+
+        document.createElement('LI')
+    }
+// ===================
+// End Samuel Notes
+// ===================
+
 function displayWeekForecast(data) {
     var latitude = data.coord.lat;
     var longitude = data.coord.lon;
@@ -108,6 +228,17 @@ function displayWeekForecast(data) {
     // Fetches the 8-Day forecast and populates the HTML to the cooresponding cards
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=imperial&appid=${weatherAPIKey}`)
         .then(response => response.json())
+        // ===================
+        // Samuel Maddox Notes
+        // ===================
+        // This function displayWeekForecast(data) defines a parameter 'data' that is a varaible 
+        // that can be used anywhere within this function. Below the .then(data) also defines a 
+        // parameter 'data' that may conflict with the 'data' variable defined in the above scope.
+        // Be carefull to use unique names within a certain scope else you may come across 
+        // unexpected errors in the future 
+        // ===================
+        // End Samuel Notes
+        // ===================
         .then(data => {
 
             //Current Day
@@ -174,8 +305,19 @@ function displayWeekForecast(data) {
             cardTemp7.innerHTML = `Temperature: ${tempValue7} °F`;
             cardHum7.innerHTML = `Humidity: ${humidValue7}%`;
         });
-
 };
+
+// ===================
+// Samuel Maddox Notes
+// ===================
+// This function does work because for 2 reasons. First, it should be written as follows:
+    $("#recentSearchItemID").on('click', function () {
+        // ...
+    })
+// Second, #recentSearchItemID doesn't exist on any element in the index.html
+// ===================
+// End Samuel Notes
+// ===================
 
 $(document).on('click', "#recentSearchItemID", function () {
     getCityWeather(this.dataset.city);
